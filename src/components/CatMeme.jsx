20b77e2fe3d22c18d10d3082a6f5332130f7c410
Edots112 <erik.strahle94@gmail.com>
2024-06-25 22:47:03 +0200

    update

--- src/components/CatMeme.jsx
+++ src/components/CatMeme.jsx
@@ -5,7 +5,7 @@ import splashImg from '../assets/Villy/splash.png';
 
 const CatMeme = () => {
     const [selected, setSelected] = useState({
-        background: '33.png',
+        background: '2.png',
         character: '1.png',
         face: '',
         frontaccessory: '',
@@ -14,19 +14,19 @@ const CatMeme = () => {
 
     const [images, setImages] = useState({
         background: [
-            "29.png", "30.png", "31.png", "32.png", "33.png", "34.png", "35.png", "36.png", "37.png",  
+            "2.png", "3.png" ,"4.png" ,"5.png" ,"6.png" ,
         ],
         character: [
-            "1.png",
+            "1.png", "24.png", "27.png"
         ],
         face: [
-            "19.png", "20.png", "21.png", "22.png", "23.png", "24.png", "25.png", "26.png",  
+             "20.png", "21.png", "22.png", "23.png",  "26.png",  
         ],
         frontAccessory: [
-            "2.png", "4.png", "6.png", "8.png", "9.png", "12.png"
+            "16.png", "17.png",  "19.png", "25.png", "28.png"
         ],
         hat: [
-            "3.png", "7.png", "10.png", "14.png", "15.png", "16.png", "17.png", "18.png", "27.png", "28.png"
+            "7.png", "8.png", "9.png", "10.png", "11.png", "12.png", "13.png", "14.png", "15.png",
         ]
     });
 
